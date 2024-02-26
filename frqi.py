66c77673bdf262bfcb1e1f09ed3e0f3fe433d443
gltonic <103551094+gltonic@users.noreply.github.com>
2024-02-26 08:34:00 +0500

    Add files via upload

--- frqi.py
+++ frqi.py
@@ -356,3 +356,117 @@ def calculate_ssim(image1, image2):
     ssim = structural_similarity(img1_array, img2_array, multichannel=False, data_range=255)
 
     return ssim
+
+
+def counts_to_statevector(counts):
+    # Calculate the total number of counts
+    total_counts = sum(counts.values())
+    
+    # Determine the number of qubits from the length of the binary strings in the dictionary keys
+    num_qubits = len(list(counts.keys())[0])
+    
+    # Initialize an array to hold the state vector data with appropriate length
+    statevector_data = np.zeros(2**num_qubits, dtype=complex)
+
+    # Iterate through each key-value pair in the counts dictionary
+    for key, value in counts.items():
+        # Convert the binary key to an integer index
+        index = int(key, 2)
+        
+        # Calculate the square root of the probability and assign it to the corresponding state vector element
+        statevector_data[index] = np.sqrt(value / total_counts)
+
+    return statevector_data
+
+
+def neuman(state, list_qubit):
+    mtx = partial_trace(state,list_qubit)
+    ent = entropy(mtx)
+    return ent
+
+
+
+def create_statevector(image, qubits):
+    # Prepare the image for encoding using the specified number of qubits
+    image = image_preparation(image, qubits)
+    
+    # Calculate the angles for encoding the prepared image
+    angles = get_angles(image, qubits)
+    
+    # Initialize an array to hold the state vector data
+    statevector = np.zeros(2 ** qubits, dtype=np.complex128)
+    
+    # Calculate the number of angles
+    N = len(angles)
+    
+    # Encode the angles into the state vector using cosine and sine functions
+    for i in range(len(angles)):
+        statevector[i] = np.cos(angles[i])
+        statevector[i + N] = np.sin(angles[i])
+    
+    # Normalize the state vector by dividing by 2^(n), where n is (qubits-1)/2
+    n = (qubits - 1) / 2
+    statevector /= 2 ** n
+
+    return statevector
+
+def encode_image_neqr(image):
+    # Convert the image to a NumPy array
+    img_np = np.array(image)
+
+    # Calculate the qubit dimension
+    n = int(np.log2(img_np.shape[0]))
+    qubit = 2 * n + 8
+    
+    # Calculate the number of bits needed for position encoding
+    for_two = len(bin(img_np.shape[0]**2-1)) - 2
+    
+    # Initialize a zero-filled array of complex numbers with size 2**qubit
+    encoded_state = np.zeros(2**qubit, dtype=complex)
+
+    # Fill the array and set corresponding elements to 1
+    for x_idx in range(img_np.shape[0]):
+        for y_idx in range(img_np.shape[1]):
+            pixel_val = img_np[x_idx, y_idx]
+            position = y_idx * img_np.shape[1] + x_idx
+            encoded_state[combine_values(pixel_val, position, for_two)] = 1
+
+    # Normalize the array
+    encoded_state /= np.linalg.norm(encoded_state)
+
+    return encoded_state
+
+def combine_values(pixel_val, position, n):
+    # Convert pixel_val to a binary string
+    pixel_binary = format(pixel_val, '08b')
+    # Convert position to a binary string of a given length n
+    position_binary = format(position, '0' + str(n) + 'b')
+    # Concatenate binary strings
+    combined_binary = pixel_binary + position_binary
+    # Convert the concatenated binary string to a decimal number
+    combined_decimal = int(combined_binary, 2)
+    return combined_decimal
+
+def decode_neqr(encoded_image):
+    # Calculate the dimension of the image
+    image_size = int(len(encoded_image) // 256)
+    x_lim = int(np.sqrt(image_size))
+    
+    # Create an array for the decoded image
+    decoded_image = np.zeros((image_size), dtype=int)
+
+    # Extract nonzero indices and their values
+    nonzero_indices = np.nonzero(encoded_image)[0]
+    n = len(bin(len(encoded_image)-1))-2
+    
+    # Decode and fill the image
+    for i in nonzero_indices:
+        position_binary = format(i, '0' + str(n) + 'b')
+        pixel_val = position_binary[:8]  
+        pixel_position = position_binary[8:]  
+        pixel_val_decimal = int(pixel_val, 2)  
+        pixel_position_decimal = int(pixel_position, 2)  
+        decoded_image[pixel_position_decimal] = pixel_val_decimal
+
+    # Reshape the decoded image to its original dimensions
+    return decoded_image.reshape((x_lim, x_lim), order='F')
